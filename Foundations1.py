a8b7edef4f6b9fd3fec7bfa4783bedea61c2f9a5
Ritesh Rohilla <65576858+riteshrohilla@users.noreply.github.com>
2022-10-04 22:32:40 +0530

    Create Foundations1.py

---
+++ Foundations1.py
@@ -0,0 +1,136 @@
+// let admin;
+// let name;
+// name = 'john';
+// admin = name;
+
+// alert(admin);
+
+// let OurPlanetName = 'Earth';
+// let CurrentVisitor = 'Ritesh Rohilla';
+// alert(OurPlanetName)
+// alert(CurrentVisitor)
+
+// const birthday = '18.04.1982';
+// const age = someCode(birthday);
+
+// let x = 5;
+// x++;
+// let z = x ** 2;
+// alert(z);
+
+// alert(10/2);
+
+// let x = 0.9
+// let y = 0.1; 
+// alert(x + y);
+// alert(x);
+// alert(y);
+
+// let x = (0.2 * 10 + 0.1 * 10) / 10;
+// alert(x);
+
+
+// let x = "100";
+// let y = "10";
+// let z = x + y;
+// alert(z);
+
+// let x = 100 / "Apple";
+// isNaN(x);
+// alert(isNaN);
+
+// const myInt = 5;
+// const myFloat = 6.67;
+// alert((typeof myInt));
+// alert((typeof myFloat));
+
+
+// let myNumber = "74";
+// myNumber += 3;
+
+// alert(myNumber);
+// alert(typeof myNumber);
+
+// alert(5 + 10 * 3);
+
+// num2 = 50;
+// num1 = 10;
+// alert((num2 + num1) / (8 + 2));
+
+// let num1 = 4;
+// num1++;
+// alert(num1);
+// x=3;
+// alert(x += 4);
+
+// let x = 1;
+// x = -x;
+// alert(x);
+
+
+// alert( 5 % 2)
+// alert( 8 % 3)
+
+// let s = "my " + "string";
+// alert(s);
+
+// let x = 1;
+// alert( +x);
+// let y = -2;
+// alert(+y);
+
+// alert( + true);
+// alert( +"")
+
+// let apples = '23';
+// let oranges = '89';
+// alert(+apples + +oranges);
+
+// let a, b, c;
+
+// a = b = c = 2 + 2;
+
+// alert( a ); // 4
+// alert( b ); // 4
+// alert( c ); // 4
+
+// let counter = 1;
+// alert( 2 * counter );
+// alert(counter++);
+
+// let a = 1, b = 1;
+// let c = ++a;
+// let d = b++;
+
+// alert(c);
+// alert(d);
+
+// let a = 2;
+// let x= 1 + (a *= 2);
+// alert(x);
+
+// let a = prompt("First number?", 1);
+// let b = prompt("Second number?", 2);
+
+// alert(+a + +b); // 12
+
+// console.log(23 + 97)
+
+// console.log((4+6+9) / 77 )
+
+// let a = 10;
+// console.log(9 * a)
+
+// let b = 7 * a
+// console.log(b)
+
+// const max = 57;
+// let actual = max - 13;
+// let percentage = actual / max;
+// alert(percentage);
+
+let name = "John";
+
+alert(`hello, ${name}!`);
+
+alert(`the result is ${1+2}`);
