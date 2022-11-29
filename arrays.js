e408d6ff748ac7eca75415958958742d992b3a75
Ritesh Rohilla <65576858+riteshrohilla@users.noreply.github.com>
2022-11-29 22:31:52 +0530

    Create arrays.js

---
+++ arrays.js
@@ -0,0 +1,54 @@
+// Arrays 
+
+// const cars = ["saab", "Volva", "BMW"];
+
+// console.log(cars.length);
+// console.log(cars.sort());
+// const person = ["Ritesh", "Rohilla", 24];
+// console.log(person[0]);
+
+// const person2 = {firstname:"Ritesh", lastname:"Rohilla", age:24};
+// console.log(person2.lastname);
+// // cars[0] = "Bugatti";
+// // let car = cars[2]
+// // console.log(car);
+// // console.log(cars[0]);
+
+
+
+// const fruits = ["Banana", "Orange", "Mango"];
+// let flen = fruits.length;
+
+// for (let i = 0; i < flen; i++) {
+  
+// }
+
+
+// const fruits = ["Banana", "Orange", "Apple"];
+// let type = typeof fruits;
+// Array.isArray(fruits);
+// console.log(type);
+// console.log(isArray);
+
+
+//  JS Array Methods
+
+// toString();
+// join();
+//pop();
+//push();
+//shift();
+//unshift();
+//length();
+//concat(); // merging two arrays
+// splice(position for new element, which item to delete);
+
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// fruits[0] = "Kiwi";
+// console.log(fruits.unshift("Lemon"));
+// console.log(fruits);
+
+// const fruits = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
+// const citrus = fruits.slice(2);
+// console.log(fruits);
+// console.log(citrus);
