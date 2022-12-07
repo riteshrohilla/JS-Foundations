1914f8cc9e2161807e6a6a7ef0e6d9509cc6b766
riteshrohilla <65576858+riteshrohilla@users.noreply.github.com>
2022-12-07 14:39:11 +0530

    Create JS-Object_Literals.js

---
+++ JS-Object_Literals.js
@@ -0,0 +1,25 @@
+// //JS Object Literals
+
+const Person = {firstName: "Ritesh", lastName: "Rohilla"}
+console.log(Person);
+
+const physicalAttributes = {
+  height: 162,
+  weight: 62,
+  isFunny: true,
+  favColors: ['Blue', 'Black']
+}
+console.log(physicalAttributes)
+
+console.log(physicalAttributes["isFunny"]);
+console.log(Person.firstName);  
+
+
+const midterms = {ritesh: 96, ritika: 67}
+console.log(midtermsN)
+
+const comments = [
+  {username: "Ritesh", text: "lolololol", votes: 9},
+  {username: "Rohilla", text: "glub glub", votes: 2312}
+]
+console.log(comments[1].text);
